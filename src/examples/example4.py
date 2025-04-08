8ed7310ff8df1ea21abee89459a6b4de5842f35d
Tomás Lima <tomas.lima@ext.ec.europa.eu>
2025-04-08 15:00:06 +0000

    wip: add example with taxonomies

---
+++ src/examples/example4.py
@@ -0,0 +1,9 @@
+from openmisp.datasets import Taxonomies
+
+pap_tag = Taxonomies.PAP.RED.tag
+confidence_tag = Taxonomies.MISP.CONFIDENCE_LEVEL.USUALLY_CONFIDENT.tag
+
+print(pap_tag)
+print(confidence_tag)
+
+pass
